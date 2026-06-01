7e0bf30981c2d22ad687185c403ad98c33ea4f4f
hnguyen29son <hnguyen29@ssis.edu.vn>
2026-06-01 08:37:04 +0700

    Added function calls for halfcourt line and center logo

--- my_picture.py
+++ my_picture.py
@@ -7,32 +7,8 @@ def draw_picture(width, height):
     
     # Fill the background
     sg.fill_background("#F1CC84")
-    
-    # Halfcourt line
-    sg.set_outline_color("white")
-    sg.draw_line(300,0,300,400, 2)
-
-    # draw circle for logo
-    sg.set_line_thickness(10)
-    sg.set_fill_color("#DC8535")
-    sg.set_outline_color("#34569B")
-    sg.fill_circle(300, 200, 60)
-
-    # Draw Y
-    sg.set_outline_color("black")
-    sg.draw_line(300,250,300,195, 8)
-    sg.draw_line(270,160,300,200, 8)
-    sg.draw_line(330,160,300,200, 8)
-
-    # Draw N
-    sg.draw_line(260,175,260,225, 8)
-    sg.draw_line(260,180,280,220, 8)
-    sg.draw_line(280,175,280,225, 8)
-
-    # Draw K
-    sg.draw_line(320,175,320,225, 8)
-    sg.draw_line(320,200,340,225, 8)
-    sg.draw_line(320,200,340,175, 8)
+    sg.half_court_line()
+    sg.knicks_logo()
     
     #Right side of the court (sonny)
     
